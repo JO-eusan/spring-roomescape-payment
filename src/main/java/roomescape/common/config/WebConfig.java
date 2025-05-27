fd898aa5813b692b7d0609fd2744c7df1674dbaf
Jin409 <sualng123@gmail.com>
2025-05-27 19:41:08 +0900

    feat: 토스 페이 연동을 위한 HttpClient 추가

--- src/main/java/roomescape/common/config/WebConfig.java
+++ src/main/java/roomescape/common/config/WebConfig.java
@@ -1,12 +1,15 @@
 package roomescape.common.config;
 
 import java.util.List;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import roomescape.presentation.interceptor.AdminAuthInterceptor;
 import roomescape.presentation.resvoler.LoginMemberArgumentResolver;
+import roomescape.presentation.support.TossPaymentWithRestClient;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -19,6 +22,13 @@ public class WebConfig implements WebMvcConfigurer {
                      AdminAuthInterceptor adminAuthInterceptor) {
         this.loginMemberArgumentResolver = loginMemberArgumentResolver;
         this.adminAuthInterceptor = adminAuthInterceptor;
+    }
+
+    @Bean
+    public TossPaymentWithRestClient tossPaymentWithRestClient() {
+        return new TossPaymentWithRestClient(
+                RestClient.builder().baseUrl("https://api.tosspayments.com/v1/payments").build()
+        );
     }
 
     @Override
---
+++ src/main/java/roomescape/presentation/support/TossPaymentWithRestClient.java
@@ -0,0 +1,33 @@
+package roomescape.presentation.support;
+
+import java.util.Base64;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestClient;
+import roomescape.dto.request.TossPaymentConfirmDto;
+import roomescape.dto.response.TossPaymentConfirmResponseDto;
+
+public class TossPaymentWithRestClient {
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String AUTHORIZATION_SCHEME= "Basic ";
+
+    @Value("${security.toss.payment.secret-key}")
+    private String secretKey;
+
+    private final RestClient restClient;
+
+    public TossPaymentWithRestClient(RestClient restClient) {
+        this.restClient = restClient;
+    }
+
+    public TossPaymentConfirmResponseDto requestConfirmation(TossPaymentConfirmDto tossPaymentConfirmDto) {
+        String encodedSecretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+
+        return restClient.post()
+                .uri("/confirm")
+                .header(AUTHORIZATION_HEADER, AUTHORIZATION_SCHEME + encodedSecretKey)
+                .body(tossPaymentConfirmDto)
+                .retrieve()
+                .body(TossPaymentConfirmResponseDto.class);
+    }
+}
