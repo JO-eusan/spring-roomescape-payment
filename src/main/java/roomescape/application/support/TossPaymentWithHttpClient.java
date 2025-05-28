502a9682f5b67b06a1953cf35b13544cadeb97bc
Jin409 <sualng123@gmail.com>
2025-05-28 14:59:56 +0900

    refactor: TossPaymentWithHttpClient 로 변경

--- src/main/java/roomescape/application/support/TossPaymentWithHttpClient.java
+++ src/main/java/roomescape/application/support/TossPaymentWithHttpClient.java
@@ -3,7 +3,7 @@ package roomescape.application.support;
 import roomescape.dto.request.TossPaymentConfirmDto;
 import roomescape.dto.response.TossPaymentConfirmResponseDto;
 
-public interface TossPaymentService {
+public interface TossPaymentWithHttpClient {
 
     TossPaymentConfirmResponseDto requestConfirmation(TossPaymentConfirmDto tossPaymentConfirmDto);
 
